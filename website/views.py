f708fd83c52add764beddc5309c10f74c281dbd4
HassarEl <mido.hass.31@gmail.com>
2024-06-15 10:31:34 +0100

    end show parte

--- website/views.py
+++ website/views.py
@@ -2,8 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from .forms import SignUpFrom
+from .models import Record
+
 
 def home(request):
+    records = Record.objects.all()
+
     #check to see if loging in
     if request.method == 'POST':
         username = request.POST['username']
@@ -18,7 +22,7 @@ def home(request):
             messages.success(request, "There Was An Error Loggin In, Please Try Again...")
             return redirect('home')
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records':records})
 
 
 def logout_user(request):
